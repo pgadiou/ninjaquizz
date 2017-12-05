9954adeb8693c72e3b69e3d3eefbc9494ef3699b
Tanguy Le Baud <tanguy.lebaud@gmail.com>
2017-12-05 16:09:49 +0100

    finetuning

--- app/controllers/quiz_answers_controller.rb
+++ app/controllers/quiz_answers_controller.rb
@@ -2,8 +2,14 @@ class QuizAnswersController < ApplicationController
 
   def create
     @quiz_answer = QuizAnswer.new(quiz_answer_params)
-    @quiz_answer.answer_id = answer.id
+    @answer = Answer.find(params[:value])
+    @quiz_answer.answer_id = @answer.id
     @quiz_answer.save
+    #check if answer is correct
+
+    #if yes increment score
+
+
   end
 
   private
