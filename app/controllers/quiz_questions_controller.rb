dec5d092a759110e5b3b50ac7ae33ef67ff29fa8
Tanguy Le Baud <tanguy.lebaud@gmail.com>
2017-12-05 15:20:26 +0100

    quiz_answer controller + show quiz_question + new and create quiz_answer

--- app/controllers/quiz_questions_controller.rb
+++ app/controllers/quiz_questions_controller.rb
@@ -1,2 +1,10 @@
 class QuizQuestionsController < ApplicationController
+
+  def show
+    @quiz_question = QuizQuestion.find(params[:id])
+    @answers = @quiz_question.question.answers
+    @quiz_answer = QuizAnswer.new
+
+  end
+
 end
