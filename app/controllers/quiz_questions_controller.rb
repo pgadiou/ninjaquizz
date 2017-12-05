43a568831a983399918a9f0f2ee37e28d8db4ce8
Philippe Gadiou <philippe.gadiou@gmail.com>
2017-12-05 16:10:44 +0100

    seed works

--- app/controllers/quiz_questions_controller.rb
+++ app/controllers/quiz_questions_controller.rb
@@ -1,2 +1,64 @@
 class QuizQuestionsController < ApplicationController
+class ServicesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: :index
+  # skip_after_action :verify_authorized, only: :index
+  before_action :set_services, only: [:index]
+
+  def index
+      @services = Service.all
+  end
+
+ def new
+    @service = Service.new
+    authorize @service
+  end
+
+  def create
+    @service = Service.new(service_params)
+    @service.user_id = current_user.id
+    authorize @service
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+  def show
+    # @location = params[:location]
+    @booking = Booking.new
+    authorize @service
+  end
+
+  def edit
+      authorize @service
+  end
+
+  def update
+    authorize @service
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @service
+    @service.destroy
+    redirect_to new_service_path
+  end
+
+private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:name, :category, :description, :location, :distance, :price, :photo)
+  end
+
+end
+
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,4 +1,5 @@
 class Round < ApplicationRecord
   belongs_to :quiz
   belongs_to :category
+  has_many :quiz_questions, dependent: :destroy
 end
