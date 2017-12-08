08d0211e6106d2ce4f2401835fec24e3675bb6d4
Philippe Gadiou <philippe.gadiou@gmail.com>
2017-12-08 11:45:36 +0100

    scroll deleted

--- app/assets/javascripts/scroll.js
+++
@@ -1,8 +0,0 @@
-function scrollLastTeamIntoView() {
-  const teams = document.querySelectorAll('.team');
-  const lastTeam = teams[teams.length - 1];
-
-  if (lastTeam !== undefined) {
-    lastTeam.scrollIntoView();
-  }
-}
