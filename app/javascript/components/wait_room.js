9fd31ce8868f5a96badebc0bd4a29860358e1e1b
Tanguy Le Baud <tanguy.lebaud@gmail.com>
2017-12-14 17:15:38 +0100

    change CSS on card creation

--- app/javascript/components/wait_room.js
+++
@@ -1,8 +0,0 @@
-const access = document.getElementById("access-info")
-
-const show_waiting_room = () => {
-  access.classList.remove("main")
-  access.classList.add("secondary");
-}
-
-access.addEventListener("click", show_waiting_room);
