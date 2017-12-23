b9468cab76dd24e47a94adb5bec1b8ffe47769cf
Philippe Gadiou <philippe.gadiou@gmail.com>
2017-12-23 12:07:10 +0100

    seed test api

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,72 +24,72 @@ choix_multiple.save!
 customized = Category.new(name: "custom")
 customized.save!
 
-# api_category = Category.new(name: "API")
-# api_category.save!
+api_category = Category.new(name: "API")
+api_category.save!
 
-# new_quiz = Quiz.new(name: "random question from api",
-#               no_of_rounds: 1,
-#               time_per_question: 10,
-#               pin_number: 123456,
-#               user: barman)
-# new_quiz.save!
+new_quiz = Quiz.new(name: "random question from api",
+              no_of_rounds: 1,
+              time_per_question: 10,
+              pin_number: 56789,
+              user: barman)
+new_quiz.save!
 
 
-# new_round = Round.new(
-# no_of_questions: 3,
-# quiz: new_quiz,
-# category: api_category,
-# )
-# new_round.save!
+new_round = Round.new(
+no_of_questions: 5,
+quiz: new_quiz,
+category: api_category,
+)
+new_round.save!
 
 
 # # test avec l'API
 
-# require 'open-uri'
-# require 'json'
+require 'open-uri'
+require 'json'
 
-# url = 'https://opentdb.com/api.php?amount=50&type=multiple'
-# quiz_serialized = open(url).read
-# quiz = JSON.parse(quiz_serialized)
+url = 'https://opentdb.com/api.php?amount=50&type=multiple'
+quiz_serialized = open(url).read
+quiz = JSON.parse(quiz_serialized)
 
 # # création d'une question avec l'API
 
-# (0..2).each do |j|
-# new_question = Question.new(
-# content: quiz["results"][j]["question"],
-# category: api_category,
-# type: choix_multiple
-# )
-# new_question.save!
+(0..4).each do |j|
+  new_question = Question.new(
+  content: quiz["results"][j]["question"],
+  category: api_category,
+  type: choix_multiple
+  )
+  new_question.save!
 
 # #associer les questions au round du quiz
 
-# new_quiz_question = QuizQuestion.new(
-# question: new_question,
-# round: new_round
-# )
-# new_quiz_question.save!
+  new_quiz_question = QuizQuestion.new(
+  question: new_question,
+  round: new_round
+  )
+  new_quiz_question.save!
 
 # # création de la bonne réponse avec l'API
 
-# new_correct_answer = Answer.new(
-# content: quiz["results"][j]["correct_answer"],
-# is_correct: true,
-# question: new_question
-# )
-# new_correct_answer.save!
+  new_correct_answer = Answer.new(
+  content: quiz["results"][j]["correct_answer"],
+  is_correct: true,
+  question: new_question
+  )
+  new_correct_answer.save!
 
 # # création des mauvaises réponses avec l'API
 
-# (0..2).each do |i|
-# new_incorrect_answer = Answer.new(
-# content: quiz["results"][j]["incorrect_answers"][i],
-# is_correct: false,
-# question: new_question
-# )
-# new_incorrect_answer.save!
-# end
-# end
+  (0..4).each do |i|
+    new_incorrect_answer = Answer.new(
+    content: quiz["results"][j]["incorrect_answers"][i],
+    is_correct: false,
+    question: new_question
+    )
+    new_incorrect_answer.save!
+  end
+end
 
 question_1 = Question.new(content: "Dans combien de villes peut-on apprendre à coder avec le Wagon ?",
   )
