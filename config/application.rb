27856ab1c0f98e2dd37f9187ed406db3d05692fc
Philippe Gadiou <philippe.gadiou@gmail.com>
2018-12-12 20:47:25 +0100

    action cable config update

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Ninjaquizz
       generate.helper false
     end
 
-    config.action_view.embed_authenticity_token_in_remote_forms = true
+    # config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
