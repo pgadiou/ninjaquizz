0212d57408acdae52488d0260aa4fe74c03ab95f
Philippe Gadiou <philippe.gadiou@gmail.com>
2018-12-12 19:13:14 +0100

    try to fix puma

--- config/puma.rb
+++ config/puma.rb
@@ -66,11 +66,11 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#   # Worker specific setup for Rails 4.1+
+#   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+#   ActiveRecord::Base.establish_connection
+# end
 
 on_worker_boot do
   # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
