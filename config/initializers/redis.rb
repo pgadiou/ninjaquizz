71ea112461e0a3611b210fde8462ef51e61f816e
Philippe Gadiou <philippe.gadiou@gmail.com>
2018-12-14 21:41:09 +0100

    test redisclud redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(:url => ENV["redis://rediscloud:BiwpLqWkbE8TSPgk@redis-17704.c6.eu-west-1-1.ec2.cloud.redislabs.com:17704"])
+if ENV["redis://rediscloud:BiwpLqWkbE8TSPgk@redis-17704.c6.eu-west-1-1.ec2.cloud.redislabs.com:17704"]
+    $redis = Redis.new(:url => ENV["redis://rediscloud:BiwpLqWkbE8TSPgk@redis-17704.c6.eu-west-1-1.ec2.cloud.redislabs.com:17704"])
+end
