8cc0b2c0ed032a9357e6c4e8cc17832b9fa1b965
Philippe Gadiou <philippe.gadiou@gmail.com>
2018-12-14 21:48:56 +0100

    test url removed from cable.yml

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-if ENV["redis://rediscloud:BiwpLqWkbE8TSPgk@redis-17704.c6.eu-west-1-1.ec2.cloud.redislabs.com:17704"]
-    $redis = Redis.new(:url => ENV["redis://rediscloud:BiwpLqWkbE8TSPgk@redis-17704.c6.eu-west-1-1.ec2.cloud.redislabs.com:17704"])
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
 end
